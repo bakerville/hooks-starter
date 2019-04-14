88eabd4b08ce6b052fa5b278095da6238e9cf7fa
rlauderdale <rjlauderdale26@gmail.com>
2019-04-14 17:15:40 -0500

    toying around with hooks
    trying out the useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Toggle from './Toggle';
 
 const App = () => {
+
+  // general formula: const [value, setValue] = useState(initialState);
+  const [name, setName] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle />
+      <form onSubmit= {e => {
+        e.prevenetDefailt();
+        formSubmit(name, setName);
+      }}>
+        <input type="text" onChange={e => setName(e.target.value)} value={name}/>
+        <button>Submit</button>
+      </form>
+      
     </div>
   );
 };
+
+const formSubmit =(value, setValue)=> {
+  console.log(`email sent to ${value}!`);
+  setValue('');
+};
+
 
 export default App;
