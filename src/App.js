3f50c8de4f857d034c94adf179c0daabbcabe78a
bakerville <thebakeriscomingforu@gmail.com>
2019-04-15 16:09:21 -0500

    tried useEffect
    a quick demo with useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Toggle from './Toggle';
 
 const App = () => {
-
   // general formula: const [value, setValue] = useState(initialState);
   const [name, setName] = useState('');
+
+  // if using a class to do this: componentdidmount and componentdidupdate would be needed
+  useEffect(()=> {
+    document.title = name;
+  })
+
 
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
       <form onSubmit= {e => {
-        e.prevenetDefailt();
-        formSubmit(name, setName);
+        e.prevenetDefailt();        
       }}>
         <input type="text" onChange={e => setName(e.target.value)} value={name}/>
         <button>Submit</button>
@@ -22,10 +26,7 @@ const App = () => {
   );
 };
 
-const formSubmit =(value, setValue)=> {
-  console.log(`email sent to ${value}!`);
-  setValue('');
-};
+
 
 
 export default App;
