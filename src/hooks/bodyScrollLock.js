0ffb0b2a15747f267d401f2f87d6ca8d7ccb9b81
rlauderdale <rjlauderdale26@gmail.com>
2019-04-17 07:28:08 -0500

    used cleanup()
    added a cleanup() function to the useBodyScrollLock() hook

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -2,8 +2,17 @@ import {useLayoutEffect } from 'react';
 
 function useBodyScrollLock(){
   useLayoutEffect(() => {
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = 'hidden';
-  });
+
+    // return function cleanup() {
+    //   document.body.style.overflow = '';
+    // }
+    // can do an arrow function too
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    }
+  },[]);
 }
 
 export {useBodyScrollLock};
